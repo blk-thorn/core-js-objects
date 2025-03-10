a5e1e047b47239710b6a1d703d7b77a81d7520bb
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-03-10 18:28:08 +0300

    feat: add makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -131,8 +131,21 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const positions = Object.values(lettersObject).flat();
+  if (positions.length === 0) {
+    return '';
+  }
+  const maxPosition = Math.max(...positions);
+  const wordArray = new Array(maxPosition + 1).fill('');
+
+  Object.entries(lettersObject).forEach(([letter, letterPositions]) => {
+    letterPositions.forEach((position) => {
+      wordArray[position] = letter;
+    });
+  });
+
+  return wordArray.join('');
 }
 
 /**
